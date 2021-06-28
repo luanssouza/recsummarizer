87e448255899943d552567d0e2a0fc77e452a6a9
LuanSSouza <luan.souza27@hotmail.com>
2021-06-28 12:45:47 -0300

    removing unitilized files

--- src/embedding/embeddings.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-modelBERT = SentenceTransformer('bert-large-nli-stsb-mean-tokens')
-
-def sentences_embeddings(sentences):
-    return modelBERT.encode(sentences)
