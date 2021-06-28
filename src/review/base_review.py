9d2a223155a9f20719f8d64a2e09915387c7aa5f
LuanSSouza <luan.souza27@hotmail.com>
2021-06-27 23:25:23 -0300

    adding review implementations

---
+++ src/review/base_review.py
@@ -0,0 +1,76 @@
+from sentence import Sentence as Sentence
+
+from collections import Counter
+
+
+class BaseReview(object):
+    def __init__(self):
+        self._id = 0
+        self._file_name = ""
+        self._sentences = []
+        self._number_of_sentences = 0
+        self._occurrences_of_each_aspect = Counter()
+        self._aspects = []
+        self._average_sentiment = 0
+        self._raw_review = ""
+        self._nouns_occurrences = Counter()        
+    
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def sentences(self):
+        return self._sentences
+    
+    @property
+    def number_of_sentences(self):
+        return self._number_of_sentences
+
+    @number_of_sentences.setter
+    def number_of_sentences(self, number):
+        self._number_of_sentences = number
+
+    @property
+    def occurrences_of_each_aspect(self):
+        return self._occurrences_of_each_aspect
+
+    @property
+    def average_sentiment(self):
+        return self._average_sentiment
+
+    @property
+    def file_name(self):
+        return self._file_name
+    
+    @property
+    def nouns_occurrences(self):
+        return self._nouns_occurrences
+
+    @property
+    def raw_review(self):
+        return self._raw_review
+
+
+    def review_extractor(self, file): 
+        """
+        Given a xml file that represents a review, this function instantiates 
+        review objects (and consequently, sentence objects) by parsing the xml tree.
+        For each review object, is computed it's xml name, it's sentences, number of sentences,
+        average sentiment, and occurrences of nouns. The aspects aren't instantiated in
+        this function, but in the module Movie.py.
+
+        
+        Args: 
+            file (Path): the file containing a raw single review
+
+        Returns:
+            None
+
+        """    
+        
+        pass
---
+++ src/review/dat_review.py
@@ -0,0 +1,79 @@
+from review.base_review import BaseReview
+
+from sentence import Sentence as Sentence
+
+from collections import Counter
+import string
+
+from corenlp_protobuf import Document, parseFromDelimitedString
+
+
+class DatReview(BaseReview):
+    def __init__(self, file):
+        self.super()
+        
+        self.review_extractor(file)
+        
+    def review_extractor(self, file): 
+        
+        filename = file.name.split(".")
+        filename = filename[0] + ".dat"
+
+        self._file_name = filename
+        
+        # https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+        # NN 	Noun, singular or mass
+     	# NNS 	Noun, plural
+     	# NNP 	Proper noun, singular
+    	# NNPS 	Proper noun, plural
+        
+        desired_pos=["NN", "NNS", "NNP", "NNPS"]
+       
+
+        #first person pronouns should be compared in lower case:
+        first_person_pronoun = ["i","we", "us","me","my","mine", "our", "ours", "myself", "ourselves"]
+        
+        review = file
+        with open(review, 'rb') as f:
+            buf = f.read()
+        doc = Document()
+        parseFromDelimitedString(doc, buf)
+
+
+        punctuation = [i for i in string.punctuation]
+        for s in doc.sentence:
+            word_counter = 0
+            sentiment_value = s.annotatedParseTree.sentiment
+            sentiment = s.sentiment
+            new_sentence = Sentence.Sentence(s.sentenceIndex, sentiment_value, sentiment, filename)
+            self._number_of_sentences += 1
+            sentiment_value = int(sentiment_value)            
+            self._average_sentiment += sentiment_value
+
+            for t in s.token:
+                new_sentence.add_token(t.word)
+                if t.word in punctuation:
+                    continue
+
+                word_counter += 1
+
+                current_word = t.word.lower()
+                
+                if current_word in first_person_pronoun:
+                    #personal opinion is setted to True:
+                    new_sentence.personal_opinion = True
+                
+                if t.pos in desired_pos:
+                    new_sentence.add_noun(current_word)  
+                    self._nouns_occurrences[current_word] += 1
+
+            new_sentence.number_of_tokens = word_counter
+            self._sentences.append(new_sentence)
+            self._raw_review += new_sentence.__str__()
+
+        if self._number_of_sentences == 0:
+            return False
+
+        self._average_sentiment = self._average_sentiment / self._number_of_sentences
+        
+        return True
