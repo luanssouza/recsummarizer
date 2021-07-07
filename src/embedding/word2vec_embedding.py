c0b5f031827c209bbdd9a23728ab284cd33341d7
LuanSSouza <luan.souza27@hotmail.com>
2021-07-07 00:54:38 -0300

    implementing word2vec_embedding

---
+++ src/embedding/word2vec_embedding.py
@@ -0,0 +1,25 @@
+from embedding.base_embedding import Embedding
+
+import gensim.downloader
+from gensim.utils import tokenize
+
+class Word2VecEmbedding(Embedding):
+    def __init__(self, model_name: str):
+        self.__model = gensim.downloader.load(model_name)
+
+    def sentence_embedding(self, sentence: str):
+        tokens = tokenize(sentence, True)
+        embedding = []
+
+        for t in tokens:
+            if self.__model.has_index_for(t):
+                if len(embedding) == 0:
+                    embedding = self.__model[t]
+                else:
+                    embedding = embedding + self.__model[t]
+
+        return embedding
+
+    def sentences_embeddings(self, sentences: list):
+        embeddings = [self.sentence_embedding(s) for s in sentences]
+        return embeddings
