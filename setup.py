7b3bb490a8da5fa94600bbb8ea33bb9669c12f83
LuanSSouza <luan.souza27@hotmail.com>
2022-09-28 20:58:44 -0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='RecSummarizer',
+      version='1.0',
+      description='Summarization framework for Recommender Systems',
+      author='Luan Souza',
+      url='https://github.com/luanssouza/recsummarizer',
+      packages=find_packages()
+     )
