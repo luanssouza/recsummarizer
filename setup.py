fd0d1c686a5cba3c5bede65ec876c0aac81cac0f
LuanSSouza <luan.souza27@hotmail.com>
2022-09-28 22:10:56 -0300

    adding install requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,15 @@ setup(name='RecSummarizer',
       description='Summarization framework for Recommender Systems',
       author='Luan Souza',
       url='https://github.com/luanssouza/recsummarizer',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=[
+            'scipy>=1.7.0',
+            'numpy>=1.20.3',
+            'pandas>=1.2.4',
+            'corenlp_protobuf>=3.8.0',
+            'nltk>=3.6.2',
+            'gensim>=4.0.1',
+            'sentence_transformers>=1.2.0',
+            'scikit-learn>=0.24.2'
+      ]
      )
